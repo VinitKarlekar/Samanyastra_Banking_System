ad62d271a8c5f4bcfcfdb572a15c9718df3d9f6e
VinitKarlekar <vinitkarlekar84474@gmail.com>
2025-04-01 21:21:10 +0530

    security key

--- bankapplications/settings.py
+++ bankapplications/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q^6vqj&gyz_dk=6x-vd2iyudlr98^8!$k5_hyp^7=ffdp8so&*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -136,7 +136,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'hariom.sharma1628@gmail.com'  
-EMAIL_HOST_PASSWORD = 'ijgj jowl lxnq rtdm'
+EMAIL_HOST_USER = ''  
+EMAIL_HOST_PASSWORD = ''
 
 LOGIN_URL = "/bank_login/"
